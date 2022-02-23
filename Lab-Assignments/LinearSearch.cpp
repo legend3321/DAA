d80234870ba178f5fe0ffc49ca0c958174b95d3a
Ujjwal Chaudhary <40447532+legend3321@users.noreply.github.com>
2022-02-23 10:15:53 +0530

    Add files via upload

---
+++ Lab-Assignments/LinearSearch.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cout << " Total Test Cases : ";
+    cin >> t;
+    for (int j = 0; j < t; j++)
+    {
+        cout << "Test Case NO : " << j + 1 << endl
+             << "------------------" << endl;
+
+        int n;
+        cout << "Size of array : ";
+        cin >> n;
+
+        int A[n];
+
+        cout << "Enter Elements : ";
+        for (int i = 0; i < n; i++)
+            cin >> A[i];
+
+        int key, passes = 0, flag = 0;
+        cout << "Enter Key : ";
+        cin >> key;
+        for (int i = 0; i < n; i++)
+        {
+            passes++;
+            if (key == A[i])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1)
+        {
+            cout << "Key Found " << endl;
+        }
+        else
+            cout << "key Not found bro" << endl;
+
+        cout << "Passes : " << passes << endl;
+    }
+}
